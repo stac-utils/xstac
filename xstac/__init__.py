f078c9b5aca0cc2d9516e630cf743d6ccefde9d8
Tom Augspurger <taugspurger@microsoft.com>
2023-10-14 16:35:44 -0500

    RLS: 1.2.0 (#39)

--- xstac/__init__.py
+++ xstac/__init__.py
@@ -4,6 +4,6 @@ xstac
 from ._xstac import xarray_to_stac, fix_attrs
 from . import _kerchunk as kerchunk
 
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 
 __all__ = ["xarray_to_stac", "fix_attrs", "kerchunk"]
